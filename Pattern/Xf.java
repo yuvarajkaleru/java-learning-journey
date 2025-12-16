f73fa85b6a76bb7b16b1e9cf1dc322b4ad442776
yuvarajkaleru <yuvarajvanjari1@gmail.com>
2025-12-16 10:23:49 +0530

    New pattern Problems

---
+++ Pattern/Xf.java
@@ -0,0 +1,17 @@
+public class Xf {
+    int n=5;
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                if(j==i){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern/Xg.java
@@ -0,0 +1,11 @@
+public class Xg {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=0;i<n;i++){
+            for(int j=n;j>i;j--){
+                System.out.print("* ");
+            }
+        System.out.println("  ");
+        }
+    }
+}
