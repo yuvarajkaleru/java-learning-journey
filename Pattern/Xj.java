583eb377e09223d6ece1e699f31f0ade8b7aeb73
yuvarajkaleru <yuvarajvanjari1@gmail.com>
2025-12-18 22:46:22 +0530

    modified

--- Pattern/Xj.java
+++ Pattern/Xj.java
@@ -1,8 +1,8 @@
-public class Xi {
+public class Xj {
     public static void main(String[] args) {
         int n=5;
         for(int i=1;i<=n;i++){
-            for(int j=n; j>=i; j--){
+            for(int j=1; j<=i; j++){
                 System.out.print(j + " ");
             }
             System.out.println();
--- Pattern/Xk.java
+++ Pattern/Xk.java
@@ -1,9 +1,11 @@
-public class Xi {
+public class Xk {
     public static void main(String[] args) {
         int n=5;
+        int k=1;
         for(int i=1;i<=n;i++){
-            for(int j=n; j>=i; j--){
-                System.out.print(j + " ");
+            for(int j=1; j<=i; j++){
+                System.out.print(k + " ");
+                k++;
             }
             System.out.println();
         }
